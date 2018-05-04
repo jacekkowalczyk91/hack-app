a016c52382812257dc10bf174dfc58e5c7c7e618
jacekkowalczyk91 <kowalczykjacek91@gmail.com>
2018-05-04 20:20:12 +0200

    fetch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import HackView from "./Components/HackView/HackView";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <HackView/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Components/HackView/HackView.js
+++ src/Components/HackView/HackView.js
@@ -1,10 +1,23 @@
 import React from 'react'
 
 class HackView extends React.Component {
+
+    state = {
+        data: {}
+    }
+
+    componentDidMount() {
+        fetch(`https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty`)
+            .then(response => response.json())
+            .then(data => this.setState({data}))
+    }
+
     render() {
         return(
             <div>
-
+                {
+                    console.log(this.state.data)
+                }
             </div>
         )
     }
